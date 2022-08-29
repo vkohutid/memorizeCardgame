8a5f4463fb7d69cf6ac8051b51e34f12f817f7f3
Vitalii Kohut <vkohut@intelliarts.com>
2022-08-29 20:36:16 +0300

    Initial Commit

---
+++ Shared/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  Shared
+//
+//  Created by Apex Woot on 29.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
